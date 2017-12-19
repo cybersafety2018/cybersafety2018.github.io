bf1e5fc8070621fc814454dd87b4eebf0ca246d6
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2017-12-19 11:46:37 -0700

    copying everything from thr 2016 website

---
+++ includes/menu1.js
@@ -0,0 +1,21 @@
+document.write("<table class=\"menu\">");
+document.write("<tr>");
+document.write("<td>");
+document.write("<nav class=\"main-nav\">");
+document.write("<ul class=\"main-nav-ul\">");
+document.write("<li><a>Home</a></li>");
+document.write("<li><a href=\"committees\">Committees</a></li>");
+document.write("<li><a href=\"call_for_contributions\">Call for Contributions</a></li>");
+document.write("<li><a href=\"program\">Program</a></li>");
+document.write("<li><a href=\"important_dates\">Important Dates</a></li>");
+document.write("<li><a href=\"registration\">Registration</a></li>");
+document.write("<li><a href=\"travel\">Travel</a></li>");
+document.write("<li><a href=\"abstracts\">Abstracts</a></li>");
+document.write("<li><a href=\"welcome_message\">Chairs' Welcome Message</a></li>");
+document.write("</ul>");
+document.write("</nav>");
+	
+document.write("</td>");
+	
+document.write("</tr>");
+document.write("</table>");
---
+++ includes/title3.js
@@ -0,0 +1,20 @@
+document.write("<table style=\"width:100%\">");
+document.write("<tr>");
+document.write("<td >	");
+document.write("<a target=\"_tab\" href=\"http://cikm2016.cs.iupui.edu/\" ><img src=\"../../figures/cikm_logo.jpg\" width=\"100%\"/></a>");
+document.write("</td>");
+
+
+document.write("<td align=\"center\">");
+document.write("<h1>CyberSafety 2016</h1>");
+document.write("<h3>The First ACM International Workshop on Computational Methods for CyberSafety </h3>");
+document.write("<h3>Co-located with <a target=\"_tab\" href=\"http://cikm2016.cs.iupui.edu/\" >CIKM 2016</a> </h3>");
+document.write("<h3>October 28, 2016</h3>");
+document.write("<h3>Indianapolis, IN, USA</h3>");
+	
+document.write("</td>");
+document.write("<td>");
+	
+document.write("</td>");
+document.write("</tr>");
+document.write("</table>");
