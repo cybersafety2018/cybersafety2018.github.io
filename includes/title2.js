6aa2716c652ba11e4d32ace858371867d5eab904
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2017-12-22 12:33:34 -0700

    edits and misspells

--- includes/title2.js
+++ includes/title2.js
@@ -7,7 +7,7 @@ document.write("</td>");
 
 document.write("<td align=\"center\">");
 document.write("<h1>CyberSafety 2018</h1>");
-document.write("<h3>The Third Workshop on Computational Methods in CyberSafety, Online Harrassment and Misinformation</h3>");
+document.write("<h3>The Third Workshop on Computational Methods in CyberSafety, Online Harassment and Misinformation</h3>");
 document.write("<h3><a target=\"_tab\" href=\"https://www2018.thewebconf.org/\" >The Web Conference 2018 </a></h3>");
 document.write("<h3>April 23, 2018</h3>");
 document.write("<h3>Lyon, France</h3>");
