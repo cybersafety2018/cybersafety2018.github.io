175b37aa801c291a2266b58844b0e55651988c03
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2018-01-08 12:38:08 -0700

    workshop dates changed

--- includes/title2.js
+++ includes/title2.js
@@ -9,7 +9,7 @@ document.write("<td align=\"center\">");
 document.write("<h1>CyberSafety 2018</h1>");
 document.write("<h3>The Third Workshop on Computational Methods in CyberSafety, Online Harassment and Misinformation</h3>");
 document.write("<h3><a target=\"_tab\" href=\"https://www2018.thewebconf.org/\" >The Web Conference 2018 </a></h3>");
-document.write("<h3>April 23, 2018</h3>");
+document.write("<h3>April 24, 2018</h3>");
 document.write("<h3>Lyon, France</h3>");
 	
 document.write("</td>");
